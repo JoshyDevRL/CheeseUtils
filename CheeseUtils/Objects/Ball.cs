f44dbebc7f7187d0b3c1dd32a5a292aae6dd7b52
JoshyDev <100446100+JoshyDevRL@users.noreply.github.com>
2023-01-09 21:10:55 +0100

    Ball.cs Fix

--- CheeseUtils/Objects/Ball.cs
+++ CheeseUtils/Objects/Ball.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using CheeseUtils;
 using CheeseUtils.Math;
 using rlbot.flat;
@@ -15,9 +10,15 @@ namespace CheeseUtils
         public const float MaxSpeed = 6000.0f;
         public const float Radius = 93.15f;
 
+        public static Ball MainBall { get { return new Ball(Location, Velocity, AngularVelocity); } }
+
         public static Vec3 Location { get; private set; }
         public static Vec3 Velocity { get; private set; }
         public static Vec3 AngularVelocity { get; private set; }
+
+        public Vec3 location;
+        public Vec3 velocity;
+        public Vec3 angularVelocity;
 
         public static BallPrediction Prediction { get; private set; }
 
@@ -27,7 +28,14 @@ namespace CheeseUtils
             Velocity = Vec3.Zero;
             AngularVelocity = Vec3.Zero;
         }
-        
+
+        public Ball(Vec3 location, Vec3 velocity, Vec3? angularVelocity = null)
+        {
+            this.location = location;
+            this.velocity = velocity;
+            this.angularVelocity = angularVelocity ?? Vec3.Zero;
+        }
+
         public static void Update(BallInfo info)
         {
             Location = info.Physics.Value.Location.HasValue ? new Vec3(info.Physics.Value.Location.Value) : Location;
